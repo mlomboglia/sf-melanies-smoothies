3d96c96e8c4b70cb31cefafc7abbc93aa130243b
Marcos Lombog <hello@marcoslombog.com>
2024-07-18 13:12:34 +0100

    data table

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,12 +19,13 @@ st.write("The name on your smoothie will be:", name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', my_dataframe, max_selections=5
 )
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 if (ingredients_list):
     ingredients_string = ''
